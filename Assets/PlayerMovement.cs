14f27671539bb1532b9658084ea9dce86ea12a17
Chiara Sanviti <chiara637288@gmail.com>
2024-06-15 17:04:00 +0200

    Movement 1

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public CharacterController2D controller;
+
+    float horizzontalMove = 0f;
+
+    public float runSpeed = 40f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+       horizzontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate() // al posto di essere chiamato ogni frame, può fare un controllo ogni x
+    {
+        controller.Move(horizzontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
